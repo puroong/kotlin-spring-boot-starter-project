1f7ea54df1daa1196ee5736b4395c60071d031a0
BumSoo Oh <skfk0135@gmail.com>
2020-01-24 13:56:43 +0900

    init project

---
+++ src/main/kotlin/com/example/starterproj/StarterprojApplication.kt
@@ -0,0 +1,11 @@
+package com.example.starterproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarterprojApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarterprojApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/starterproj/StarterprojApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.starterproj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarterprojApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
